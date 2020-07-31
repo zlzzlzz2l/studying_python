cb926ca95750cb1beb93a41ce275d4b241e8a915
ahyun <zlzzlzz2l@naver.com>
2020-08-01 05:09:08 +0900

    edit calc

--- calc.py
+++ calc.py
@@ -10,117 +10,70 @@ def cal(n1, op, n2):
         result = n1 / n2
     return result
 
+def binary(list1):
+    bin_val = ['0', '1']
+    for b in list1:
+        if b in bin_val:
+            pass
+        elif b not in bin_val:
+            print("잘못 입력하셨습니다.")
+            return num_input()
+
+def octal(list1):
+    oct_val = ['0', '1', '2', '3', '4', '5', '6', '7']
+    for o in list1:
+        if o in oct_val:
+            pass
+        elif o not in oct_val:
+            print("잘못 입력하셨습니다.")
+            return num_input()
+
+def hexadecimal(list1):
+    hex_val = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f']
+    for h in list1:
+        if h in hex_val:
+            pass
+        elif h not in hex_val:
+            print("잘못 입력하셨습니다.")
+            return num_input()
+
+def num_input():
+    global com, num1, operator, num2
+    num1, operator, num2 = input("연산자와 피연산자를 입력하시오. : ").split()
+    num1_com = list(num1)
+    num2_com = list(num2)
+    com = num1_com + num2_com
+    return com
+
 if __name__ == '__main__': 
 
     num_type = 3
     
     while num_type != 0:
         num_type = input("진수를 입력하세요. (bin: 1, oct: 2, dec : 3, hex: 4, 종료: 0) : " )
-        num1, operator, num2 = input("연산자와 피연산자를 입력하시오. : ").split()
         if num_type == '1':
-            num1_com = list(num1)
-            num2_com = list(num2)
-            com = num1_com + num2_com
-            for b in com:
-                i = 0
-                while i < len(b):
-                    if '0' in b[i]:
-                        break
-                    elif '1' in b[i]:
-                        break
-                    else:
-                        print("잘못 입력하셨습니다.")
-                        num1, operator, num2 = input("연산자와 피연산자를 입력하시오. : ").split()
-                        break
-                    i = i + 1
-                break
+            num_input()
+            binary(com)
             num1 = int(num1, 2)
             num2 = int(num2, 2)
             cal(num1, operator, num2)
             print(bin(num1), operator, bin(num2), '=', bin(result))
         elif num_type == '2':
-            num1_com = list(num1)
-            num2_com = list(num2)
-            com = num1_com + num2_com
-            for o in com:
-                i = 0
-                while i < len(o):
-                    if '0' in o[i]:
-                        break
-                    elif '1' in o[i]:
-                        break
-                    elif '2' in o[i]:
-                        break
-                    elif '3' in o[i]:
-                        break
-                    elif '4' in o[i]:
-                        break
-                    elif '5' in o[i]:
-                        break
-                    elif '6' in o[i]:
-                        break
-                    elif '7' in o[i]:
-                        break
-                    else:
-                        print("잘못 입력하셨습니다.")
-                        num1, operator, num2 = input("연산자와 피연산자를 입력하시오. : ").split()
-                        break
-                    i = i + 1
-                break
+            num_input()
+            octal(com)
             num1 = int(num1, 8)
             num2 = int(num2, 8)
             cal(num1, operator, num2)
             print(oct(num1), operator, oct(num2), '=', oct(result))
         elif num_type == '3':
+            num_input()
             num1 = int(num1)
             num2 = int(num2)
             cal(num1, operator, num2)
             print(num1, operator, num2, '=', result)
         elif num_type == '4':
-            num1_com = list(num1)
-            num2_com = list(num2)
-            com = num1_com + num2_com
-            for h in com:
-                i = 0
-                while i < len(h):
-                    if '0' in h[i]:
-                        break
-                    elif '1' in h[i]:
-                        break
-                    elif '2' in h[i]:
-                        break
-                    elif '3' in h[i]:
-                        break
-                    elif '4' in h[i]:
-                        break
-                    elif '5' in h[i]:
-                        break
-                    elif '6' in h[i]:
-                        break
-                    elif '7' in h[i]:
-                        break
-                    elif '8' in h[i]:
-                        break
-                    elif '9' in h[i]:
-                        break
-                    elif 'a' in h[i]:
-                        break
-                    elif 'b' in h[i]:
-                        break
-                    elif 'c' in h[i]:
-                        break
-                    elif 'd' in h[i]:
-                        break
-                    elif 'e' in h[i]:
-                        break
-                    elif 'f' in h[i]:
-                        break
-                    else:
-                        print("잘못 입력하셨습니다.")
-                        num1, operator, num2 = input("연산자와 피연산자를 입력하시오. : ").split()
-                        break
-                    i = i + 1
-                break
+            num_input()
+            hexadecimal(com)
             num1 = int(num1, 16)
             num2 = int(num2, 16)
             cal(num1, operator, num2)
