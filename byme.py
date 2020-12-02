62b487c8d003667b7a3ea4502bbe57a5ccffbe23
ahyun <zlzzlzz2l@naver.com>
2020-12-03 03:51:11 +0900

    study module

---
+++ byme.py
@@ -0,0 +1,5 @@
+def sign():
+    print("이 프로그램은 ahyun Kim에 의해 만들어졌습니다.")
+    print("인스타그램 : instagram.com/zlzzlzz2l")
+    print("이메일 : zlzzlzz2l@naver.com")
+ 
--- introduction/age.py
+++ introduction/age.py
@@ -1,3 +1,11 @@
 class agePackage:
     def detail(self):
         print("24")
+        
+if __name__ == "__main__":
+    print("agePackage 모듈 직접 실행")
+    print("이 문장은 모듈을 직접 실행할 때에만 실행된다.")
+    name_to = agePackage()
+    name_to.detail()
+else:
+    print("agePackage 외부에서 모듈 호출")
